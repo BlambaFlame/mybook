9265564f1252b0da27ab9e923abd5a35ff7871c9
Aishat Zakirov <lickey8crp@gmail.com>
2022-06-13 17:12:24 +0300

    Added shared kernel

---
+++ MyBook.SharedKernel/SharedHelpers/CollectionHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBook.SharedKernel.SharedHelpers;
+
+public static class CollectionHelper<T>
+{
+    public static bool Contains(ICollection<T> collection, Func<T, bool> predicate)
+    {
+        return collection.Where(predicate).Any();
+    }
+}
