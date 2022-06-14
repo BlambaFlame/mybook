ecb92965dbd579f7cb2ccd4bc5f80ecd0724fbb7
Aishat Zakirov <lickey8crp@gmail.com>
2022-06-14 10:16:19 +0300

    Обновлены модели

---
+++ MyBook.Core/Entities/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBook.Entities
+{
+    public partial class Test
+    {
+        public int TestId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
